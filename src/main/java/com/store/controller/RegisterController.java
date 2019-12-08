89f759542e86bbdf043120c2e36846c92479c318
franklin <franklin_bourguignon@hotmail.com>
2019-12-08 18:07:07 -0300

    Controller para o cadastro

---
+++ src/main/java/com/store/controller/RegisterController.java
@@ -0,0 +1,24 @@
+package com.store.controller;
+
+import com.store.dto.RegisterRequestDTO;
+import com.store.dto.ResponseDTO;
+import com.store.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@RequestMapping("/register")
+public class RegisterController {
+
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequestDTO createRegistrationRequest) {
+        userService.save(createRegistrationRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO("Usuário registrado com sucesso!"));
+    }
+}

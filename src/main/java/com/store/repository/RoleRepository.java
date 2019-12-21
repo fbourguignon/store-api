e528658c3193c71e4e250b132abd4d658b3b0100
franklin <franklin_bourguignon@hotmail.com>
2019-12-21 01:23:20 -0300

    Repository das roles

--- src/main/java/com/store/repository/RoleRepository.java
+++ src/main/java/com/store/repository/RoleRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, UUID> {
     Optional<Role> findByType(RoleType roleType);
 }

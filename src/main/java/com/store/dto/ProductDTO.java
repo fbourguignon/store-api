1a9ae00e739eebd97f031119808fa9c80b7a40c3
franklin <franklin_bourguignon@hotmail.com>
2019-12-21 01:13:17 -0300

    Criação de um DTO para as requisições de produto

---
+++ src/main/java/com/store/dto/ProductDTO.java
@@ -0,0 +1,13 @@
+package com.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ProductDTO {
+
+    private String name;
+
+    private String description;
+}

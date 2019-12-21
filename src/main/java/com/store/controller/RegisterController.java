21a59cc724e3d3c64616dbed94fd7103d1aa9783
franklin <franklin_bourguignon@hotmail.com>
2019-12-21 01:19:56 -0300

    Controller de cadastro

--- src/main/java/com/store/controller/RegisterController.java
+++ src/main/java/com/store/controller/RegisterController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RestController
 @RequestMapping("/register")
 public class RegisterController {
 

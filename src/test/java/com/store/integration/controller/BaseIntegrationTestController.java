822d1c23fdc39b6c497f8f0ddf77fd5c6643d58c
Franklin <franklin_bourguignon@hotmail.com>
2020-02-09 16:54:08 -0300

    Tornando a classe base para testes de integração abstrata.

--- src/test/java/com/store/integration/controller/BaseIntegrationTestController.java
+++ src/test/java/com/store/integration/controller/BaseIntegrationTestController.java
@@ -19,7 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class BaseIntegrationTestController {
+public abstract class BaseIntegrationTestController {
 
     @Autowired
     protected TestRestTemplate restTemplate;

12577113386d36460cbfe0228e110ca0ec1ad5a0
franklin <franklin_bourguignon@hotmail.com>
2020-02-13 21:50:22 -0300

    Concluindo os testes do ProductController.

--- src/main/java/com/store/repository/ProductRepository.java
+++ src/main/java/com/store/repository/ProductRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
+    Product findByName(String name);
 }
--- src/test/java/com/store/integration/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/store/integration/controller/ProductControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.store.integration.controller;
 
 import com.store.dto.ProductDTO;
+import com.store.dto.ResponseDTO;
 import com.store.integration.response.RestResponsePage;
 import com.store.model.Product;
 import com.store.repository.ProductRepository;
@@ -10,20 +11,24 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.*;
 
+import java.util.UUID;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 
 public class ProductControllerIntegrationTest extends AbstractControllerIntegrationTests {
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Test
     public void mustLisWithPaginationProducts() {
 
         ParameterizedTypeReference<RestResponsePage<Product>> responseType = new ParameterizedTypeReference<RestResponsePage<Product>>() {
         };
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", getJWTAdmin());
-        headers.add("Content-Type", "application/json");
+        HttpHeaders headers = getAuthenticatedHeaders();
 
         HttpEntity<PageRequest> pageRequest = new HttpEntity<>(headers);
         ResponseEntity<RestResponsePage<Product>> response = restTemplate.exchange("/products", HttpMethod.GET, pageRequest/*httpEntity*/, responseType);
@@ -36,18 +41,54 @@ public class ProductControllerIntegrationTest extends AbstractControllerIntegrat
     @Test
     public void mustCreateProduct() {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", getJWTAdmin());
-        headers.add("Content-Type", "application/json");
+        HttpHeaders headers = getAuthenticatedHeaders();
 
         ProductDTO productDTO = ProductDTO.builder().name("ProdutoCreate").description("Description").build();
         HttpEntity<ProductDTO> request = new HttpEntity<>(productDTO, headers);
 
-        ResponseEntity<Product> response = restTemplate.exchange("/products", HttpMethod.POST, request/*httpEntity*/, Product.class);
-        Product product = response.getBody();
+        restTemplate.exchange("/products", HttpMethod.POST, request/*httpEntity*/, Product.class);
 
-        assertNotNull(product.getId());
+        Product product = productRepository.findByName("ProdutoCreate");
+
+        assertNotNull(product);
         assertEquals(product.getName(), productDTO.getName());
+        assertEquals(product.getDescription(), productDTO.getDescription());
+    }
+
+    @Test
+    public void mustDeleteProduct() {
+
+        HttpHeaders headers = getAuthenticatedHeaders();
+
+        HttpEntity<ProductDTO> request = new HttpEntity<>(headers);
+
+        restTemplate.exchange("/products/aa3355b8-da98-478f-8a9a-aef10739e416", HttpMethod.DELETE, request/*httpEntity*/, ResponseDTO.class);
+
+        assertTrue(productRepository.findById(UUID.fromString("aa3355b8-da98-478f-8a9a-aef10739e416")).isEmpty());
+    }
+
+    @Test
+    public void mustEditProduct() {
+
+        HttpHeaders headers = getAuthenticatedHeaders();
+        ProductDTO productDTO = ProductDTO.builder().name("ProductEdited").description("Description").build();
+
+        HttpEntity<ProductDTO> request = new HttpEntity<>(productDTO,headers);
+
+        restTemplate.exchange("/products/aa3355b8-da98-478f-8a9a-aef10739e416", HttpMethod.PUT, request/*httpEntity*/, ResponseDTO.class);
+
+        Product product = productRepository.findById(UUID.fromString("aa3355b8-da98-478f-8a9a-aef10739e416")).get();
+
+        assertNotNull(product);
+        assertEquals(product.getName(), productDTO.getName());
+        assertEquals(product.getDescription(), productDTO.getDescription());
+    }
+
+    private HttpHeaders getAuthenticatedHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", getJWTAdmin());
+        headers.add("Content-Type", "application/json");
+        return headers;
     }
 
 }
